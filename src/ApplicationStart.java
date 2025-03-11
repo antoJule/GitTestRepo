e1b5fa6373a9ea287280420a65f2649f03c5885d
AntoJule <antjul95@gmail.com>
2025-03-11 10:11:52 +0100

    New Commmit

--- src/ApplicationStart.java
+++ src/ApplicationStart.java
@@ -4,7 +4,7 @@ public class ApplicationStart {
 
         int sum = 0;
         for(int i = 0; i < 100; i ++){
-            if (i % 2 == 0){
+            if (i % 3 == 0){
                 sum+=i;
             }
         }
